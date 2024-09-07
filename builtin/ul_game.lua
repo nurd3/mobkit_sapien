6598dd3e99624fb5cf89de44aae006e6ceb56b1e
nurdle <50721698+nurd3@users.noreply.github.com>
2024-09-07 06:57:39 -0400

    Unlit support + minor reorganization

---
+++ builtin/ul_game.lua
@@ -0,0 +1,22 @@
+mobkit_sapien.register_job("mobkit_sapien:hunter", {
+    description = S"Monster Hunter",
+    items = {
+        [1] = {"ul_mobs:eye"},
+        [2] = {},
+        [3] = {"ul_magic:health"},
+        [4] = {},
+        [5] = {"ul_basic:knife", "ul_magic:regeneration"},
+        [6] = {"ul_basic:sword", "ul_mobs:big_eye"}
+    }
+})
+mobkit_sapien.register_job("mobkit_sapien:trafficker", {
+    description = S"Monster Trafficker",
+    items = {
+        [1] = {"ul_mobs:eye"},
+        [2] = {"ul_mobs:zombie", "ul_mobs:ghost", "ul_mobs:big_eye"},
+        [3] = {"ul_mobs:stalker", "ul_mobs:vampire"},
+        [4] = {"ul_mobs:skeleton"},
+        [5] = {"ul_basic:knife", "ul_mobs:lich", "ul_mobs:shadow"},
+        [6] = {"ul_basic:sword", "ul_mobs:lootglob"}
+    }
+})
